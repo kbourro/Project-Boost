57fb1d53a9f76e871d4fc1511d4981f4accd54ca
Kristofer Bourro <kbourro@gmail.com>
2019-10-17 17:39:01 +0300

    update

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,34 +4,33 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    Animator animator;
+    [SerializeField]
+    Vector3 movementVector;
+
+    [SerializeField]
+    float period = 2f;
+
+    [Range(0, 1)]
+    [SerializeField]
+    float movementFactor;
+
+    Vector3 startingPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
+        startingPos = transform.position;
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(animator.enabled)
-            animator.enabled = false;
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (!animator.enabled)
-            animator.enabled = true;
-    }
-
-    private void OnCollisionStay(Collision collision)
-    {
-        if (animator.enabled)
-            animator.enabled = false;
+        if(period<=Mathf.Epsilon) { return; }
+        float cycles = Time.time / period;
+        float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles*tau);
+        movementFactor = rawSinWave /2f +0.5f;
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
     }
 }
